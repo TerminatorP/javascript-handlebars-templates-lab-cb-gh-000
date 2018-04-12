c66af0a06e039f99fd61d37e0ec1ab325aa35326
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-12 12:03:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function init() {
   function createRecipe() {
     event.preventDefault();
+    event.stopPropagation();
     let recipePageTemplate = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML);
     document.getElementById('main').innerHTML += recipePageTemplate;
   }
